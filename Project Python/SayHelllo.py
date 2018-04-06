227e9d724a366a6dfdc67e81ba3a83b86a493d47
TonGiaBao <baoton488@gmail.com>
2018-04-06 19:54:00 +0700

    Add files via upload

---
+++ Project Python/SayHelllo.py
@@ -0,0 +1,10 @@
+import datetime
+from time import clock
+
+dt = datetime.datetime.strftime(datetime.datetime.now(),"%d-%m-%Y %H:%M:%S%z")
+print(dt)
+
+def xuat(x):
+    x = 2 *2
+    print(x)
+xuat("")
